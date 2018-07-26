e4d3791bdf26f32e63490234a069c4f2a6c5053c
Emily Welsch <emwelsch@gmail.com>
2018-07-26 16:50:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name).map do |rows|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
