ee0d93338a01e941d40d74e40a3d5f6de6e12ef6
Emily Welsch <emwelsch@gmail.com>
2018-07-26 16:59:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,24 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10
+  end
+
+  def self.first_student_in_grade_10
+  end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end 
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
